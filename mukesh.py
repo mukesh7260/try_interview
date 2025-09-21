48996f6974f145164fd6507f7e2d04a88cbd95b8
mukesh7260 <mk2648054@gmail.com>
2025-09-21 13:20:45 +0000

    message

--- mukesh.py
+++ mukesh.py
@@ -1,19 +1,9 @@
+# comprehension list using of lambda 
 
-# Methood overloading examle in python 
+# l = lambda : [i for i in range(10) if i % 2 == 0] 
+# print(l()) 
 
-class Cricket:
-    def sum(self,a = None, b = None, c = None):
-        if a != None and b != None and c != None:
-            return a + b + c 
-        elif a != None and b != None:
-            return a + b 
-        elif a != None:
-            return a 
-        else:
-            return 0 
-c = Cricket() 
-print(c.sum(2,3,4)) 
-print(c.sum(2,3)) 
-print(c.sum(2)) 
-print(c.sum())
-            
+
+
+a = lambda b ,c : b + c 
+print(a(2,3)) 
