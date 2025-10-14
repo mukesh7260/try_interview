2af1251d10cf37bf140db9dc1ae6db2076903424
mukesh7260 <mk2648054@gmail.com>
2025-10-14 03:30:00 +0000

    message

--- mukesh.py
+++ mukesh.py
@@ -1,1 +1,16 @@
 
+n = int(input("How many numbers in the list: "))
+a = []
+for i in range(n):
+    b = int(input(f"Enter element {i+1}: "))  # Convert input to int
+    a.append(b)
+    
+    if len(a) >= 2:
+        c = 3
+        d = a[0]
+        e = a[1]
+        f = d + e
+        if f == c:
+            print(d, e)
+        else:
+            print(1)
--- try.py
+++ try.py
@@ -1060,3 +1060,25 @@ for num in c:
 # | ✅ **Streaming data**             | For real-time data (e.g., logs, sensor input, file streams)     |
 # | ✅ **Performance optimization**   | Reduces memory usage and can make programs faster               |
 # | ✅ **Custom iteration logic**     | Easier than writing a class with `__iter__` and `__next__`      |
+
+
+
+
+
+# BEST IMPORTANT QUESTION FOR INTERVIEWS 
+
+
+n = int(input("How many numbers in the list: "))
+a = []
+for i in range(n):
+    b = int(input(f"Enter element {i+1}: "))  # Convert input to int
+    a.append(b)
+    if len(a) >= 2:
+        c = 3
+        d = a[0]
+        e = a[1]
+        f = d + e
+        if f == c:
+            print(d, e)
+        else:
+            print(1) 
