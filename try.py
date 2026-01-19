e4142163afa457ad082f44d9cf85b1f10ecf1534
mukesh7260 <mk2648054@gmail.com>
2026-01-19 11:02:07 +0000

    message

--- try.py
+++ try.py
@@ -1454,3 +1454,18 @@ result = [[1,4]]
 print(result[-1][1])
 
 # o/p: 4
+
+
+
+intervals = [[2,4],[4,7],[8,10]]
+result = []
+for i in intervals:
+    if not result or result[-1][1] < i[0]:
+        result.append(i) 
+    else:
+        result[-1][1] = max(result[-1][1],i[1])
+
+print(result)
+
+
+#  o/p : - [[2, 7], [8, 10]]
