ca4c4720f2d06e2e4f60cb031ea166187610db69
mukesh7260 <mk2648054@gmail.com>
2026-07-04 14:34:48 +0000

    commit

--- try.py
+++ try.py
@@ -1479,4 +1479,20 @@ for i in range(len(l)):
         max = l[i]
 print(max)
 
-# o/p: - 65
+# o/p: - 65 
+
+
+# print even number from string 
+
+s = "sdkjf78934239ie!2#$%^*f"
+l = ['0','1','2','3','4','5','6','7','8','9']
+
+for i in range(len(s)):
+    print(i,'print iiiiiiiiiiiiii')
+    print(s[i],'s[i] **********')
+    if s[i] in l:
+        if int(s[i]) % 2 == 0:
+            print(s[i], end=" ") 
+
+
+
